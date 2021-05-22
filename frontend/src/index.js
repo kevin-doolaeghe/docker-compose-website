253a7246f13c5e31e86b72922a281975220e3ff8
Kevin Doolaeghe <doolaeghe.kevin@gmail.com>
2021-05-23 00:42:35 +0200

    deleted service worker module

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,11 +4,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 import IdeaList from './IdeaList';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <IdeaList />,
   document.getElementById('root')
 );
-
-serviceWorker.unregister();
