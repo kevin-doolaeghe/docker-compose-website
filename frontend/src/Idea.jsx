13e8f265cb41e09b5390ab6e8664b2a48cb2f5da
Kevin Doolaeghe <doolaeghe.kevin@gmail.com>
2021-05-25 19:14:43 +0200

    solved bugs

--- frontend/src/Idea.jsx
+++ frontend/src/Idea.jsx
@@ -2,11 +2,12 @@ import React from "react";
 
 import "./style.css";
 
-const Idea = ({ details, onDeleteIdea }) => (
+const Idea = ({ details, onIdeaDelete }) => (
   <div className="Idea">
-    <button onClick={() => onDeleteIdea(details.id)}>X</button><h2><b>{details.title}</b></h2>
+    <h2><b>{details.title}</b></h2>
     <h4>{details.firstname} {details.lastname} - {details.date_t}</h4>
     <p>{details.content}</p>
+    <button onClick={() => onIdeaDelete(details.id)}>Supprimer l'idée</button>
   </div>
 );
 
