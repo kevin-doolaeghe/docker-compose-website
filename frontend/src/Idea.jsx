e923c4832c40fc25e1023c61316e25cd7eb21f57
Kevin Doolaeghe <doolaeghe.kevin@gmail.com>
2021-05-25 18:49:49 +0200

    css

--- frontend/src/Idea.jsx
+++ frontend/src/Idea.jsx
@@ -4,9 +4,8 @@ import "./style.css";
 
 const Idea = ({ details }) => (
   <div className="Idea">
-    <h2>{details.title}</h2>
+    <h2>{details.title} - <h4>{details.firstname} {details.lastname}</h4></h2>
     <p>{details.content}</p>
-    <h4>{details.firstname} {details.lastname}</h4>
   </div>
 );
 
